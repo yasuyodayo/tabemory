f7edb660ee95d24b61af20fbcb1cf2d2b438e62d
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-10-30 22:00:56 +0900

    トップページのurl変更

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='tabemory/')),
     path('admin/', admin.site.urls),
     path("tabemory/", include("tabemory.urls")),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
