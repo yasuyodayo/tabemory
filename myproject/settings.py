6046d3030b24d9affaf505666453012f5a7135f5
松野康世 <matsunokousei@matsunoyasuyonoMacBook-Pro.local>
2025-11-02 23:00:11 +0900

    データベース作成

--- myproject/settings.py
+++ myproject/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
